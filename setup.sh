b1635455e996f57d8b8d392a7fcc72dfb2f3d827
keeganjk <keegan.j.k@outlook.com>
2016-12-15 16:52:47 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,40 @@
+# This script sets up "anonymous" for Kali Linux.
+# Made by Keegan Kuhn
+# keegan.j.k@outlook.com
+
+tor_install()
+	echo ""
+	echo "ANONYMOUS SETUP FOR KALI LINUX ROLLING"
+	echo ""
+	read -p "Is Tor installed on this computer? [Y/n] (N if unsure) " tor
+	case $tor in
+		[Nn]* ) apt-get install tor; break;;
+		[Yy]* ) break;;
+        	* ) echo "You didn't type Y or n, aborting!"; break;;
+	esac;
+
+# Adding needed code to a Tor file
+echo "[.] Copying code to Tor configuration ..."
+echo 'VirtualAddrNetwork 10.192.0.0/10' >> /etc/tor/torrc
+echo 'TransPort 9040' >> /etc/tor/torrc
+echo 'DNSPort 53' >> /etc/tor/torrc
+echo 'AutomapHostsOnResolve 1\n' >> /etc/tor/torrc
+echo "[*] Done !"
+
+# Copying files to needed locations
+echo "[.] Copying files to designated locations ..."
+cp anonymous /usr/sbin/anonymous
+cp backbox-anonymous /etc/default/backbox-anonymous
+echo "[*] Done !"
+
+# Finish
+echo "Install should be successful!"
+echo "You can delete these install files;"
+echo "they're useless now..."
+echo "You can use anonymous by typing anonymous into the terminal"
+echo "------------------------------"
+echo "START: anonymous start "
+echo "STOP : anonymous stop  "
+echo "STAT : anonymous status"
+echo ""
+echo "If you find a bug, contact me at keegan.j.k@outlook.com"
